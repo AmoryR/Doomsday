4182da5763b97ac51da8a7d2ce1603a6bd438504
Amory <theonlygeeking@gmail.com>
2022-01-30 18:04:40 +0100

    Initial Commit

---
+++ Doomsday/DoomsdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoomsdayApp.swift
+//  Doomsday
+//
+//  Created by Amory Rouault on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DoomsdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
